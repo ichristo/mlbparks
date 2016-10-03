42d9f7dfa2eff42ba458851b3e6ddd5513b7a6d0
Isaac Christoffersen <ichristoffersen@vizuri.com>
2016-10-03 11:16:17 -0400

    Adding JUnit Test to force failure

---
+++ src/test/java/org/openshift/mlbparks/scaffoldingTest.java
@@ -0,0 +1,14 @@
+package org.openshift.mlbparks;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class scaffoldingTest {
+
+	@Test
+	public void test() {
+		fail("Forcing a Test Failure");
+	}
+
+}
